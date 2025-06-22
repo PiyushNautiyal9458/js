e8101c88bc0403cc8d4a3cc4c1f05fd0faf14c6f
PiyushNautiyal9458 <142298649+PiyushNautiyal9458@users.noreply.github.com>
2025-06-22 07:23:27 +0000

    some add

--- 01_basics/dtypes.js
+++ 01_basics/dtypes.js
@@ -5,3 +5,20 @@
 
 console.log(typeof undefined)//=>undefined
 console.log(typeof null)//=>object
+let score=22;
+console.log(typeof score)
+let nsc="2f34";
+console.log(typeof nsc);
+let nval=Number(nsc);
+console.log(typeof nval);
+console.log(nval);
+//non primitive datatype gets value in heap and we get reference of the object
+let obj={
+    name:"piyush",
+    age:24
+}
+
+let nobj=obj
+nobj.age=66
+console.log(nobj.age)
+console.log(obj.age);
