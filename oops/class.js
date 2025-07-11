c42e561d4a99cf017f4b7ae3256b2243a23d0f0f
PiyushNautiyal9458 <142298649+PiyushNautiyal9458@users.noreply.github.com>
2025-07-11 14:52:17 +0000

    class_obj

--- oops/class.js
+++ oops/class.js
@@ -18,16 +18,44 @@
 // chai.displayage()
 
 
-function demo(username,age,password){
-    this.username=username
-    this.age=age
-    this.password=password
-    return `my age is ${this.age}`
-}
+// function demo(username,age,password){
+//     this.username=username
+//     this.age=age
+//     this.password=password
+//     return `my age is ${this.age}`
+// }
 
-demo.prototype.displayname=function(){
-    return `kaise hia aap ${this.username}`
-}
+// demo.prototype.displayname=function(){
+//     return `kaise hia aap ${this.username}`
+// }
 
-const hey=new demo("piyush",34,"sbfkaj")
-console.log(hey.displayname())
+// const hey=new demo("piyush",34,"sbfkaj")
+// console.log(hey.displayname())
+
+// class user{
+//     constructor(username){
+//         this.username=username
+//     }
+
+//     callme(){
+//         return `this is my new space ${this.username}`
+//     }
+// }
+
+// class parent extends user{
+//     constructor(user,email,age){
+//         super(user)
+//         this.email=email
+//         this.age=age
+//     }
+
+//     anothercall(){
+//         return `hey ${this.age}`
+//     }
+// }
+
+// const chai=new parent("ravi","raman@gmail.com",20)
+// const chai1=new parent("aman","sam@gmail.com",90)
+// console.log(chai.callme())
+// console.log(chai1.anothercall())
+
